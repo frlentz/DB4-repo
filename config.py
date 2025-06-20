5ea5848c0a79adbf5ca98de13968c501a71fa1a0
frlentz <frederiklentzz@gmail.com>
2025-06-20 11:18:45 +0200

    Revert "FIx: update"
    This reverts commit d08ee89e49baef3b6c86fbd2ff4ea4c5434245e5.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-wifi_password = "123456789"
-wifi_id = "Frederik - iPhone 15"
-username = "frederiklentz"
-key = "aio_iGpQ50mnmSysN7QN3a9vpG3wCvww"
+wifi_password = ""
+wifi_id = ""
+username = ""
+key = ""
